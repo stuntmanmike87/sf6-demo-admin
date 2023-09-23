8adea75542ae19762d5d49a96ab5ec4a5261095d
stuntmanmike87 <mich.perigaud@gmail.com>
2023-09-23 13:21:53 +0200

    added Data Fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+//use Symfony\Component\String\Slugger\SluggerInterface;
+
+final class UserFixtures extends Fixture
+{
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordEncoder/* ,
+        private readonly SluggerInterface $slugger */
+    )
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setEmail('admin@demo.fr');
+        $admin->setPassword($this->passwordEncoder->hashPassword($admin, 'admin'));
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+        $faker = Factory::create('fr_FR');
+
+        for($usr = 1; $usr <= 5; ++$usr){//for($usr = 1; $usr <= 5; $usr++){
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setPassword($this->passwordEncoder->hashPassword($user, 'secret'));
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
+
+/*
+//@return array<string>
+public function getDependencies(): array
+{
+    return [
+        LevelFixtures::class,
+    ];
+}
+*/
