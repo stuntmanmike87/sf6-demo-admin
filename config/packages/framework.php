7a941d5d2c2aaa9f7f5cb4ebe0c409d59f3e37dd
stuntmanmike87 <mich.perigaud@gmail.com>
2024-05-09 15:42:38 +0200

    Update sf/fwk-bdle recipe

--- config/packages/framework.php
+++ config/packages/framework.php
@@ -7,16 +7,10 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->extension('framework', [
         'secret' => '%env(APP_SECRET)%',
-        'http_method_override' => false,
-        'session' => [
-            'handler_id' => null,
-            'cookie_secure' => 'auto',
-            'cookie_samesite' => 'lax',
-            'storage_factory_id' => 'session.storage.factory.native',
-        ],
-        'php_errors' => [
+        'session' => true,
+        /* 'php_errors' => [
             'log' => true,
-        ],
+        ], */
     ]);
     if ($containerConfigurator->env() === 'test') {
         $containerConfigurator->extension('framework', [
