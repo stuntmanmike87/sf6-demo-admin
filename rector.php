7f54df3c36faa0c8c6bf03419029b472922b4e13
stuntmanmike87 <mich.perigaud@gmail.com>
2024-07-03 11:55:03 +0200

    Update deps 20240703

--- rector.php
+++ rector.php
@@ -19,8 +19,6 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
 
-    //$rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
-
     // basic rules
     $rectorConfig->importNames();
     $rectorConfig->importShortClasses();
@@ -39,29 +37,9 @@ return static function (RectorConfig $rectorConfig): void {
         LevelSetList::UP_TO_PHP_83,
     ]);
 
-    // symfony rules
-    /* $rectorConfig->symfonyContainerPhp(
-        __DIR__ . '/var/cache/website/dev/App_KernelDevDebugContainer.xml'
-    );
-
-    $rectorConfig->symfonyContainerXml(
-        __DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml'
-    ); */
-
-    /* $rectorConfig->import(SymfonySetList::SYMFONY_63);
-
-    $parameters = $rectorConfig->parameters();
-    
-    $parameters->set(Option::SOURCE, [__DIR__ . '/src']);
-
-    $parameters->set(
-        Option::class,
-        __DIR__ . '/var/cache/dev/AppKernelDevDebugContainer.xml'
-    ); */
-
     $rectorConfig->sets([
         //SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
-        SymfonySetList::SYMFONY_64,//SYMFONY_70,
+        SymfonySetList::SYMFONY_71,
         SymfonySetList::SYMFONY_CODE_QUALITY,
         SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
     ]);
@@ -79,14 +57,5 @@ return static function (RectorConfig $rectorConfig): void {
         PHPUnitSetList::PHPUNIT_CODE_QUALITY,
     ]);
 
-    /* $rectorConfig->skip([
-        VarConstantCommentRector::class,
-    ]); */
-
     $rectorConfig->importShortClasses(false);
-
-    /* $rectorConfig->ruleWithConfiguration(ChangeFileLoaderInExtensionAndKernelRector::class, [
-        ChangeFileLoaderInExtensionAndKernelRector::FROM => 'yaml',
-        ChangeFileLoaderInExtensionAndKernelRector::TO => 'php',
-    ]); */
 };
